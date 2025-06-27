6d40d2e0609fa42923aef8c86134ac8066b41175
Shivani <shivani2873gupta@gmail.com>
2025-06-27 18:56:15 +0530

    add redux-persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,26 @@
-import {configureStore} from '@reduxjs/toolkit'
+import {configureStore,combineReducers} from '@reduxjs/toolkit'
 import userReducer from './user/userSlice'
+import persistReducer from 'redux-persist/es/persistReducer';
+import { version } from 'mongoose';
+import persistStore from 'redux-persist/es/persistStore';
+
+const rootReducer=combineReducers({
+    user:userReducer,
+});
+
+//this will store the user information
+const persistConfig={
+    key:'root',
+    storage,
+    version:1,
+}
+const persistReducer=persistReducer(persistConfig,rootReducer);
 
 export const store=configureStore({
-    reducer:{
-        user:userReducer,
-    },
+    // reducer:{ user:userReducer, },
+    reducer:persistReducer,
+    //below one is used to solve the error due to redux-toolkit
+    middleware:(getDefaultMiddleware)=>
+        getDefaultMiddleware({ serializableCheck:false}),
 })
+export const persistor=persistStore(store)l
