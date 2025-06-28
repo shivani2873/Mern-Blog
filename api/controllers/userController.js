ae5e592da9550e57c896c68e03f6378f321e6bf8
Shivani <shivani2873gupta@gmail.com>
2025-06-28 17:58:23 +0530

    update user API route

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,3 +1,48 @@
+import User from "../models/userModel";
+import { errorHandler } from "../utils/error.js";
+import {bcryptjs} from 'bcryptjs'
+
 export const test = (req,res)=>{
     res.json({message:'API is working!'});
 };
+
+export const updateUser=async (req,res,next)=>{
+    // console.log(req.user);
+    if(req.user.id !== req.params.userId){
+        return next(errorHandler(403,'You are not allowed to update thi user'));
+    }
+    if(req.body.password){
+        if(req.body.password.length < 6){
+            return next(errorHandler(400,'Password must be at least 6 characters'));
+        }
+        req.body.password=bcryptjs.hashSync(req.bosy.password,10);
+    }
+    if(req.body.username){
+        if(req.body.username.length < 7 || req.body.username.length>20){
+            return next(errorHandler(400,'Username must be between 7 to 20 characters'));
+        }
+        if(req.body.username.includes(' ')){
+            return next(errorHandler(400,'Username cannot contain spaces'));
+        }
+        if(req.body.username !== req.body.username.toLowerCase()){
+            return next(errorHandler(400,'Username must be in lowercase'));
+        }
+        if(req.body.username.match(/^[a-zA-Z0-9]+$/)){
+            return next(errorHandler(400,'Username can only conatin letters & numbers'));
+        }
+        try{
+            const updateUser=await User.findByIdAndUpdate(req.params.userId,{
+                $set:{
+                    username:req.bosy.username,
+                    email:req.body.email,
+                    profilePic:req.body.profilePic,
+                    password:req.body.password,
+                },
+            },{new:true});
+            const {password,...rest}=updateUser._doc;
+            res.status(200).json(rest);
+        }catch(err){
+            next(err);
+        }
+    }
+}
--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/userRoute.js';
 import authRoutes from './routes/authRoute.js'
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
@@ -13,7 +14,10 @@ mongoose.connect(process.env.Mongo).then(()=>{
 });
 
 const app=express();
+
 app.use(express.json());
+app.use(cookieParser());
+
 app.listen(3000,()=>{
     console.log('Server is running on port 3000');
 });
